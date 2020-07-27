e4d1956cfe0b108bf480026f8ca4f64fdf91d914
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-07-27 19:22:17 +0300

    Added a couple of dependencies

---
+++ src/main/java/com/testows/TestoWsApplication.java
@@ -0,0 +1,13 @@
+package com.testows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoWsApplication.class, args);
+	}
+
+}
